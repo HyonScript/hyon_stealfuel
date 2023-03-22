b4320e035f6eefe2f9d982f79e8d1d560addc589
HyonScript <123055191+HyonScript@users.noreply.github.com>
2023-03-22 17:07:52 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.weapon = "WEAPON_WRENCH" -- item need to break the car
+
+Config.hose = "hose" -- item need to steal
+
+Config.stealamount = 10 -- Steal amount
+
+Config.Locales = {
+car_break = "Press ~g~G~s~ to break the Tank",
+hand_petrolcan = "You need a Petrol Can in your hand",
+start_steal = "Press ~g~G~s~ to Start Steal Fuel",
+noitem = "You don't have ~o~Hose~s~",
+fuel_level = "Fuel Level: ",
+lowfuel = "Car Fuel level too low",
+canfull = "Petrol Can Level is Full",
+brokhose = "~r~OOOPS~s~ ~o~broken the hose~s~",
+fuelstealed = "~o~Stealed ~g~10~s~ Liter Fuel~s~", 
+}
